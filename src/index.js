b8a0c8e6bdd55b36728f5bd97ad3a784589022e7
prabushram <prabushram@yahoo.com>
2026-02-09 00:31:07 -0500

    ## US-05: Timer

--- src/index.js
+++ src/index.js
@@ -196,7 +196,10 @@ function clearScore() {
 function updateTimer() {
   // TODO: Write your code here.
   // hint: this code is provided to you in the instructions.
-  
+  if (time > 0){
+    time -= 1;
+    timerDisplay.textContent = time;
+  }
   return time;
 }
 
@@ -209,6 +212,7 @@ function updateTimer() {
 function startTimer() {
   // TODO: Write your code here
   // timer = setInterval(updateTimer, 1000);
+  timer = setInterval(updateTimer, 1000);
   return timer;
 }
 
